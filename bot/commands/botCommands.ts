a6b778362f9702ebcc0ced97847690ed1c7cfbb2
Anton Matusenco <anton@matusenco.com>
2023-12-24 21:58:42 +0200

    update packages

--- bot/commands/botCommands.ts
+++ bot/commands/botCommands.ts
@@ -2,5 +2,5 @@ import { Context, Telegraf } from 'telegraf';
 import { Update } from 'typegram';
 
 export const setBotCommands = (bot: Telegraf<Context<Update>>) => {
-    bot.command('hello', (ctx) => ctx.reply('world!'));
+    bot.command('hello', (ctx) => ctx.reply('world!!'));
 };
