4caf1c475a985b29ece16307bb911ce83861fa0e
Anton Matusenco <anton@matusenco.com>
2023-12-27 21:39:18 +0200

    simplify ts

--- bot/commands/botCommands.ts
+++
@@ -1,6 +0,0 @@
-import { Context, Telegraf } from 'telegraf';
-import { Update } from 'typegram';
-
-export const setBotCommands = (bot: Telegraf<Context<Update>>) => {
-    bot.command('hello', (ctx) => ctx.reply('world!'));
-};
--- bot/utils/throwIf.ts
+++
@@ -1,7 +0,0 @@
-export const throwIfNo = (envVariable?: string) => {
-    const variableName = Object.keys({ envVariable })[0];
-
-    if (envVariable === undefined) {
-        throw new Error(`${variableName} must be provided!`);
-    }
-}
