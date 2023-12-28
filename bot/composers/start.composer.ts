2bff6dab33ddd5b8ff00374d510ad8bc22f6f915
Anton Matusenco <anton@matusenco.com>
2023-12-28 19:56:00 +0200

    add buttons

--- bot/composers/start.composer.ts
+++ bot/composers/start.composer.ts
@@ -1,10 +1,19 @@
-import { Composer } from 'telegraf';
-import { parse } from 'yaml';
-import { readFileSync } from "fs";
-
-const locale = parse(readFileSync('../locales/en.yaml', 'utf8'));
-
+import {Composer, Markup} from 'telegraf';
 
 export const startComposer = Composer.command('start', async ctx => {
-    await ctx.reply(locale.start);
+    const message = `
+Salut, ${ctx.message.from.first_name ? ctx.message.from.first_name : "who are you?"}!
+
+Aici poți gasi orice informatie de care ai nevoie 
+in politica organizatie EcoVisio, 
+pentru orientare mai buna in bot sunt 2 directii
+Junior si Senior
+
+Tot asa se poate sa te uiti la general cu ajutorul comenzii /help`;
+    await ctx.reply(message, Markup.keyboard([
+        [
+            Markup.button.callback("😝 Junior", "😝 Junior"),
+            Markup.button.callback("🥸 Senior", "🥸 Senior")
+        ]
+    ]).resize());
 });
